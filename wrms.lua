fa849127ec1121bcab4563303e3e82520b56331f
AndrewShike <andrew.shike@regalcuttingtools.com>
2020-01-21 13:08:17 -0600

    wrm2 record works
    think share has bugs

--- wrms.lua
+++ wrms.lua
@@ -18,6 +18,7 @@ function init()
     softcut.pre_level(h, 0.0)
     softcut.rate_slew_time(h, 0.2)
     softcut.rec_level(h + 2, 1.0)
+    softcut.rate(h + 2, 1.0)
     
     for j = 1,2 do
       softcut.enable(i + j, 1)
@@ -29,7 +30,7 @@ function init()
       softcut.pan(i + j, j == 1 and -1 or 1)
       
       softcut.loop_start(i+j, wrms_loop[j].loop_start)
-      softcut.loop_start(i+j, wrms_loop[j].loop_end)
+      softcut.loop_end(i+j, wrms_loop[j].loop_end)
       softcut.position(i+j, wrms_loop[j].loop_start)
     end
   end
@@ -91,26 +92,23 @@ wrms_pages = { -- ordered pages of visual controls and actions (event callback f
           if wrms_loop[2].has_initial then -- if inital loop has been recorded
             softcut.rec(3, v) -- toggle recording
             softcut.rec(4, v)
-            
-            print("toggle")
           elseif wrms_loop[2].punch_in_time ~= nil then -- else if inital loop is being punched in, punch out
             softcut.rec(3, 0) -- stop recording but keep playing
             softcut.rec(4, 0)
             
             local lt = util.clamp(util.time() - wrms_loop[2].punch_in_time, 0, 200) -- loop time = now - when we punched-in
-            wrms_loop[2].region_end = lt -- set loop end to loop time
-            wrms_loop[2].loop_end = lt
-            softcut.loop_end(3, lt)
-            softcut.loop_end(4, lt)
-            
-            -- softcut.position(3, wrms_loop[2].loop_start)
-            -- softcut.position(4, wrms_loop[2].loop_start)
+            wrms_loop[2].region_end = lt + wrms_loop[2].region_start -- set loop end to loop time
+            wrms_loop[2].loop_end = lt + wrms_loop[2].loop_start
+            softcut.loop_end(3, wrms_loop[2].loop_end)
+            softcut.loop_end(4, wrms_loop[2].loop_end)
             
             wrms_loop[2].has_initial = true -- this is how we know we're done with the punch-in
             
           elseif v == 1 then -- else start loop punch-in
             wrms_loop[2].region_end = 201 -- set loop end to max
             wrms_loop[2].loop_end = 201
+            softcut.loop_start(3, wrms_loop[2].loop_start)
+            softcut.loop_start(4, wrms_loop[2].loop_start)
             softcut.loop_end(3, 201)
             softcut.loop_end(4, 201)
             softcut.position(3, wrms_loop[2].loop_start)
@@ -133,7 +131,7 @@ wrms_pages = { -- ordered pages of visual controls and actions (event callback f
           wrms_loop[2].punch_in_time = nil
           wrms_loop[2].has_initial = false
         
-          softcut.buffer_clear_region(wrms_loop[1].region_start,wrms_loop[1].region_end) -- clear loop region
+          softcut.buffer_clear_region(wrms_loop[2].region_start, wrms_loop[2].region_end) -- clear loop region
         end
       end
     }
@@ -173,6 +171,7 @@ wrms_pages = { -- ordered pages of visual controls and actions (event callback f
       event = function(v) 
         softcut.rate(1, 2^v)
         softcut.rate(2, 2^v)
+        wrms_loop[1].rate = 2^v
       end
     },
     e3 = {
@@ -187,14 +186,30 @@ wrms_pages = { -- ordered pages of visual controls and actions (event callback f
       label = "<<",
       value = 0,
       behavior = "momentary",
-      event = function(v, t) end
+      event = function(v, t)
+        local st = (1 + (math.random() * 0.5)) * t
+        softcut.rate_slew_time(3, st)
+        softcut.rate_slew_time(4, st)
+        
+        wrms_loop[2].rate = wrms_loop[2].rate / 2
+        softcut.rate(3, wrms_loop[2].rate)
+        softcut.rate(4, wrms_loop[2].rate)
+      end
     },
     k3 = {
       worm = 2,
       label = ">>",
       value = 0,
       behavior = "momentary",
-      event = function(v, t) end
+      event = function(v, t) 
+        local st = (1 + (math.random() * 0.5)) * t
+        softcut.rate_slew_time(3, st)
+        softcut.rate_slew_time(4, st)
+        
+        wrms_loop[2].rate = wrms_loop[2].rate * 2
+        softcut.rate(3, wrms_loop[2].rate)
+        softcut.rate(4, wrms_loop[2].rate)
+      end
     }
   },
   {
@@ -279,8 +294,8 @@ wrms_pages = { -- ordered pages of visual controls and actions (event callback f
       value = 1.0,
       range = { 0.0, 1.0 },
       event = function(v)
-        softcut.level_cut_cut(1, 3, v)
-        softcut.level_cut_cut(2, 4, v)
+        softcut.level_cut_cut(1, 3, v * wrms_pages[1].e2.value)
+        softcut.level_cut_cut(2, 4, v * wrms_pages[1].e2.value)
       end
     },
     e3 = { -- feed wrm 2 to wrm 1
@@ -289,8 +304,8 @@ wrms_pages = { -- ordered pages of visual controls and actions (event callback f
       value = 0.0,
       range = { 0.0, 1.0 },
       event = function(v) 
-        softcut.level_cut_cut(3, 1, v)
-        softcut.level_cut_cut(4, 2, v)
+        softcut.level_cut_cut(3, 1, v * wrms_pages[2].e2.value)
+        softcut.level_cut_cut(4, 2, v * wrms_pages[2].e2.value)
       end
     },
     k2 = {
