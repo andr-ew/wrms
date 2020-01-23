e7a57291ecdb0da7d69f136caa0952f47dd21a2f
AndrewShike <andrew.shike@regalcuttingtools.com>
2020-01-22 22:38:33 -0600

    docs comments

--- wrms.lua
+++ wrms.lua
@@ -1,7 +1,27 @@
 -- wrms
--- minimal, extensible dual echo looper
+-- minimal, extensible dual 
+-- echo looper
 -- v011820 @andrew
 -- llllllll.co/t/22222
+-- norns interface only
+-- 2 wrms (stereo loops) with 
+-- unique characteristics
+-- v - channel volumes plus 
+-- record toggle wrm1 is a 
+-- simple toggle, wrm2 a loop 
+-- pedal-style punch-in
+-- o - old controls how much 
+-- of the past remains as wrms 
+-- circle thru time
+-- b - 2 controls for rate/pitch 
+-- as wrms apprach light speed
+-- s - length of wrm1 and where 
+-- he begins - allows tiny bit 
+-- chopping and subtle delay 
+-- movement
+-- > - config for telepathic 
+-- inter-wrm time loop 
+-- communication
 
 include 'wrms/lib/lib_wrms'
 
