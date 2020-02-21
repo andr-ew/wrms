2cb40447388f8042e6ff74bafbf70f28a70d0b75
AndrewShike <andrew.shike@regalcuttingtools.com>
2020-02-20 18:28:22 -0600

    comments ZzzZzz

--- lib/supercut.lua
+++ lib/supercut.lua
@@ -1,66 +1,7 @@
 tabutil = require "tabutil"
-
--- temporary override
--- local SC = softcut
-
--- function SC.defaults()
---   zeros = {}
---   for i=1, SC.VOICE_COUNT do zeros[i] = 0 end
-   
---   local state = {}
---   for i=1,SC.VOICE_COUNT do
---     state[i] = {}
-     
---     state[i].enable = 0
---     state[i].play = 0
---     state[i].record = 0
-     
---     state[i].buffer = (i%2 + 1)
---     state[i].level = 0
---     state[i].pan = 0
-     
---     state[i].level_input_cut = {0,0}
---     state[i].level_cut_cut = zeros
-     
---     state[i].rate = 1
---     state[i].loop_start = (i-1)*2
---     state[i].loop_end = (i-1)*2+1
---     state[i].loop = 1
-     
---     state[i].fade_time =  0.0005
---     state[i].rec_level = 0
---     state[i].pre_level = 0
---     state[i].rec = 0
---     state[i].rec_offset = -0.00015
---     state[i].position = 0
-     
---     state[i].pre_filter_fc = 16000
---     state[i].pre_filter_dry = 0
---     state[i].pre_filter_lp = 1
---     state[i].pre_filter_hp = 0
---     state[i].pre_filter_bp = 0
---     state[i].pre_filter_br = 0
---     state[i].pre_filter_fc_mod = 1
-
---     state[i].post_filter_fc = 12000
---     state[i].post_filter_dry = 0
---     state[i].post_filter_lp = 0
---     state[i].post_filter_hp = 0
---     state[i].post_filter_bp = 0
---     state[i].post_filter_br = 0
-     
---     state[i].level_slew_time = 0.001
---     state[i].rate_slew_time = 0.001
---     state[i].phase_quant = 1
---     state[i].phase_offset = 0
---   end
---   return state
--- end
-
-
 --[[
 
-these are the guts for the supercut lib documented over this a way -> https://llllllll.co/t/supercut-lib/29526
+these are the guts for the supercut lib! it is documented over this a way -> https://llllllll.co/t/supercut-lib/29526
 
 ]]--
 
